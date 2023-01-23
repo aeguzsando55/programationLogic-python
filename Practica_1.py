91e5591427413809ad089bb23769375708b7159b
aeguzsando55 <aeguzsando55@outlook.com>
2023-01-23 13:11:43 -0600

    practica 1 finalizada

--- Practica_1.py
+++ Practica_1.py
@@ -1,12 +1,38 @@
 import numpy as np
+import math
 
+#* Operaciones aritemeticas simples
 def suma(num1, num2):
     return num1 + num2
 
-#* Operaciones aritemeticas simples
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1 , num2):
+    return num1/num2
+
+def potencia(num, n):
+    return num**n
+
+def raiz_cuadrada(num):
+    return math.sqrt(num)
+
 if __name__ == "__main__":
     print("hello world")
     num1 = int(input("Ingrese el primer numero: "))
     num2 = int(input("Ingrese el segundo numero: "))
     mi_suma = suma(num1, num2)
-    print(mi_suma)
+    print(f"la suma de {num1} y {num2} es igual a {mi_suma}")
+    mi_resta = resta(num1, num2)
+    print(f"la resta de {num1} y {num2} es igual a {mi_resta}")
+    mi_multiplicacion = multiplicacion(num1, num2)
+    print(f"el producto de {num1} y {num2} es igual a {mi_suma}")
+    mi_division = division(num1, num2)
+    print(f"la division entre  {num1} y {num2} es igual a {mi_division}")
+    mi_potencia = potencia(num1, num2)
+    print(f"{num1} a la {num2} es igual a {mi_potencia}")
+    mi_raiz = raiz_cuadrada(num1)
+    print(f"la raiz cuadrada de {num2} es igual a {mi_raiz}")
