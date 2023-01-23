035867df71215e03939ee45ca40893b32c4e9b20
aeguzsando55 <aeguzsando55@outlook.com>
2023-01-23 00:11:52 -0600

    segundo commit

--- Practica_1.py
+++ Practica_1.py
@@ -1,4 +1,12 @@
 import numpy as np
 
+def suma(num1, num2):
+    return num1 + num2
+
+#* Operaciones aritemeticas simples
 if __name__ == "__main__":
     print("hello world")
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    mi_suma = suma(num1, num2)
+    print(mi_suma)
